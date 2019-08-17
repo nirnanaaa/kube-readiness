b27bda747cab861239d0a197cedc5e1671b5bc08
nirnanaaa <flo.fk44@gmail.com>
2019-08-17 20:01:14 +0200

    Initial import

---
+++ README.md
@@ -0,0 +1,17 @@
+# Kubernetes External Pod Readiness
+
+
+## Development
+
+Startup the controller:
+
+```
+make run
+```
+
+
+Run tests:
+
+```
+make test
+```
---
+++ controllers/ingress_controller.go
@@ -0,0 +1,51 @@
+/*
+Copyright 2019 Kube Readiness.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+
+	"github.com/go-logr/logr"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	networkingv1beta1 "k8s.io/api/networking/v1beta1"
+)
+
+// IngressReconciler reconciles a Ingress object
+type IngressReconciler struct {
+	client.Client
+	Log logr.Logger
+}
+
+// +kubebuilder:rbac:groups=networking.k8s.io,resources=ingresses,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=networking.k8s.io,resources=ingresses/status,verbs=get;update;patch
+
+func (r *IngressReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
+	_ = context.Background()
+	_ = r.Log.WithValues("ingress", req.NamespacedName)
+
+	// your logic here
+
+	return ctrl.Result{}, nil
+}
+
+func (r *IngressReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&networkingv1beta1.Ingress{}).
+		Complete(r)
+}
