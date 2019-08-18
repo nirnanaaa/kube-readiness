85be901c0a16bedcdb36bd80c4a303d37dd013c5
Ivo Verberk <iverberk@xebia.com>
2019-08-18 18:02:01 +0200

    Add first version of E2E test.

---
+++ e2e/e2e.sh
@@ -0,0 +1,111 @@
+#!/bin/bash -euo pipefail
+
+# This script performs the following actions:
+#
+# 1. Create an EKS cluster with eksctl, based on the cluster.yaml file
+# 2. Deploy the AWS ALB Ingress controller
+# 3. Deploy the echoserver
+# 4. Perform a rolling update of the echoserver
+# 5. Start a k6 load-test
+
+if ! [ -x "$(command -v eksctl)" ]; then
+  echo 'Error: eksctl is not installed.' >&2
+  exit 1
+fi
+
+if ! [ -x "$(command -v k6)" ]; then
+  echo 'Error: k6 is not installed.' >&2
+  exit 1
+fi
+
+if ! [ -x "$(command -v kubectl)" ]; then
+  echo 'Error: kubectl is not installed.' >&2
+  exit 1
+fi
+
+if ! [ -x "$(command -v aws)" ]; then
+  echo 'Error: aws is not installed.' >&2
+  exit 1
+fi
+
+# Global variables
+
+AWS_PROFILE=${AWS_PROFILE:-default}
+CLUSTER_NAME=${CLUSTER_NAME:-kube-readiness}
+KUBECONFIG="./kube-config-$CLUSTER_NAME"
+ECHOSERVER_LB_DNS=""
+TG_ARN=""
+
+# Always clean up the echo server deployment and remove the kubeconfig file
+
+function finish {
+  kubectl delete --ignore-not-found=true -f ./echoserver/echoserver-deployment.yaml > /dev/null
+
+  rm -f $KUBECONFIG
+}
+trap finish EXIT
+
+# 1. Create the EKS cluster if it doesn't exist
+
+if ! eksctl -p $AWS_PROFILE get cluster $CLUSTER_NAME > /dev/null; then
+  sed "s/__CLUSTER_NAME__/$CLUSTER_NAME/g" cluster.yaml | eksctl -p $AWS_PROFILE create cluster --kubeconfig $KUBECONFIG -f -
+fi
+
+# 2. Install AWS ALB Ingress controller
+
+kubectl apply -f ./alb-ingress-controller > /dev/null
+
+# 3a. Deploy the echoserver ingress
+
+kubectl apply -f ./echoserver/echoserver-ingress.yaml > /dev/null
+
+echo -n "Waiting for loadbalancer DNS location to become available"
+while [[ -z $ECHOSERVER_LB_DNS ]]; do
+  ECHOSERVER_LB_DNS=$(kubectl get ingress -ojsonpath="{.items[*].status.loadBalancer.ingress[*].hostname}")
+  printf '.'
+  sleep 2
+done
+
+# Get ingress loadbalancer ARN
+LB_ARN=$(aws --region=eu-west-1 elbv2 describe-load-balancers --query "LoadBalancers[?DNSName==\`$ECHOSERVER_LB_DNS\`].LoadBalancerArn" --output text)
+
+# Get ingress target group ARN
+echo
+echo -n "Waiting for targetgroup"
+while [[ -z $TG_ARN ]]; do
+  TG_ARN=$(aws --region=eu-west-1 elbv2 describe-target-groups --query "TargetGroups[?contains(LoadBalancerArns, '$LB_ARN')].TargetGroupArn" --output text)
+  printf '.'
+  sleep 2
+done
+
+# Make sure the echoserver deployment has been deleted
+kubectl delete --ignore-not-found=true -f ./echoserver/echoserver-deployment.yaml > /dev/null
+
+# Wait for an empty targetgroup
+echo
+echo -n "Waiting for all loadbalancer targets to deregister"
+until [[ -z $(aws --region=eu-west-1 elbv2 describe-target-health --target-group-arn $TG_ARN --output text --query "TargetHealthDescriptions") ]]; do
+    printf '.'
+    sleep 2
+done
+
+# 3b. Deploy the echoserver
+
+kubectl apply -f ./echoserver > /dev/null
+
+# Wait for successful loadbalancer response
+echo
+echo -n "Waiting for echoserver to respond"
+until $(curl --output /dev/null --silent --head --fail http://$ECHOSERVER_LB_DNS); do
+    printf '.'
+    sleep 2
+done
+
+# 4. Perform rolling upgrade of the echoserver (1.4 -> 1.10)
+echo
+echo "Executing rolling upgrade"
+sed "s/1.4/1.10/g" ./echoserver/echoserver-deployment.yaml | kubectl apply -f - > /dev/null
+
+# 5. Start the k6 load test
+echo "Starting load test"
+ECHOSERVER_LB_DNS=$ECHOSERVER_LB_DNS k6 run k6.js
---
+++ e2e/k6.js
@@ -0,0 +1,24 @@
+import http from "k6/http";
+import { check, sleep } from "k6";
+import { Rate, Counter } from "k6/metrics";
+
+let Errors = new Rate("Errors");
+let CounterErrors = new Counter("Errors");
+
+export let options = {
+  vus: 10,
+  duration: "30s",
+  thresholds: {
+    "Errors": [ { threshold: "count == 0" } ]
+  }
+};
+
+export default function() {
+  const res = http.get(`http://${__ENV.ECHOSERVER_LB_DNS}`)
+
+  CounterErrors.add(check(res, {
+    "status is 200 (OK)": (r) => r.status == 200
+  }));
+
+  sleep(1);
+};
