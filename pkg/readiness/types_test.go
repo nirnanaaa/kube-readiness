98704cd297ef7c8e105e2fa8b92ac9c17010d88a
nirnanaaa <flo.fk44@gmail.com>
2019-08-18 16:22:35 +0200

    add endpoint map tests.

---
+++ pkg/readiness/types_test.go
@@ -0,0 +1,35 @@
+package readiness
+
+
+import (
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/types"
+)
+var _ = Describe("Readiness Types", func() {
+	Context("Ingress map", func() {
+		It("Should build and get an ingress map correctly", func() {
+			By("creating an ingress map")
+			igMap := make(IngressSet)
+
+			ingress := types.NamespacedName{
+				Name: "abc",
+				Namespace: "some-system",
+			}
+			endpointMap := igMap.Ensure(ingress)
+			Expect(endpointMap.Len()).Should(Equal(0))
+			By("adding an ingress")
+			ep := IngressEndpoint{
+				IP: "10.10.0.1",
+				Port: "80",
+				Node: "10.10.0.0",
+			}
+			endpointMap.Insert(ep)
+			Expect(endpointMap.Len()).Should(Equal(1))
+			By("removing the ingress")
+			endpointMap.Delete(ep)
+			Expect(endpointMap.Len()).Should(Equal(0))
+		})
+	})
+})
